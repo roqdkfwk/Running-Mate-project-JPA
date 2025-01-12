b77cd98c152afbd1fba283b6e866b2d4c53a2d51
유석민 <qusgh1027@naver.com>
2025-01-12 23:18:14 +0900

    fix: 사용하지 않는 import문 삭제

--- backend/src/main/java/com/suseok/run/controller/GroupController.java
+++ backend/src/main/java/com/suseok/run/controller/GroupController.java
@@ -1,33 +1,16 @@
 package com.suseok.run.controller;
 
-import java.util.List;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.suseok.run.jwtutill.AuthRequired;
-import com.suseok.run.model.dto.Board;
 import com.suseok.run.model.dto.Group;
-import com.suseok.run.model.dto.User;
-import com.suseok.run.model.service.BoardService;
 import com.suseok.run.model.service.GroupService;
-import com.suseok.run.model.service.UserService;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/group")
