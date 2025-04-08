b565b3542a15153566d3dc9b3ba1f9415f81f661
유석민 <qusgh1027@naver.com>
2025-04-08 21:34:00 +0900

    chore: GroupController URI 수정

--- backend/src/main/java/com/suseok/run/controller/GroupController.java
+++ backend/src/main/java/com/suseok/run/controller/GroupController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/group")
+@RequestMapping("/groups")
 @Tag(name = "GroupRestController", description = "그룹 CRUD")
 @RequiredArgsConstructor
 public class GroupController {
