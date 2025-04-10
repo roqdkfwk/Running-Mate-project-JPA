78d2fe24ef6cbd4c34510bc618721f80f0d66b45
smyoo <smyoo@wiezon.com>
2025-04-10 10:26:19 +0900

    feat: 그룹 리포지토리 생성

---
+++ backend/src/main/java/com/suseok/run/model/repository/GroupRepository.java
@@ -0,0 +1,9 @@
+package com.suseok.run.model.repository;
+
+import com.suseok.run.model.entity.Group;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GroupRepository extends JpaRepository<Group, Integer> {
+}
