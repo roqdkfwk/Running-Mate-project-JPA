e1ca75bdee6942142e8ad1334831853117738bef
유석민 <qusgh1027@naver.com>
2025-04-20 01:04:53 +0900

    fix: Authorization 헤더를 노출하도록 설정
    - 해당 설정을 하지 않으면 클라이언트에서
    response.headers['authorization']으로 토큰을 가져올 수 없음

--- backend/src/main/java/com/suseok/run/config/WebConfig.java
+++ backend/src/main/java/com/suseok/run/config/WebConfig.java
@@ -14,6 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Authorization", "Content-Type")
                 .exposedHeaders("Authorization")
-                .allowCredentials(true);
+                .allowCredentials(true)
+                .exposedHeaders("Authorization");
     }
 }
