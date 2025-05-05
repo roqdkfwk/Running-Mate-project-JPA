ebff83e39d122a2d9fe766dafb1eca813105cad4
유석민 <qusgh1027@naver.com>
2025-05-05 16:56:40 +0900

    feat: 댓글 조회 기능 개발
    - 게시글에 달린 댓글 목록 조회 메서드
    - 조회된 댓글 화면에 출력
    - 댓글 작성시 화면 새로고침 없이 즉시 반영

--- backend/src/main/java/com/suseok/run/model/service/CommentService.java
+++ backend/src/main/java/com/suseok/run/model/service/CommentService.java
@@ -1,10 +1,15 @@
 package com.suseok.run.model.service;
 
 import com.suseok.run.model.entity.Request.CreateCommentReq;
+import com.suseok.run.model.entity.Response.CreateCommentRes;
+
+import java.util.List;
 
 public interface CommentService {
 
-    Long createComent(Long userSeq, Long postId, CreateCommentReq createCommentReq);
+    CreateCommentRes createComment(Long userSeq, Long postId, CreateCommentReq createCommentReq);
+
+    List<CreateCommentRes> readAllComments(Long postId);
 
     void updateComment(Long userSeq, Long commentId, String content);
 
--- vue/vite.config.js
+++ vue/vite.config.js
@@ -1,16 +1,21 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    headers: {
+      // 브라우저에게 캐시하지 말라고 명시
+      'Cache-Control': 'no-store'
+    },
+    watch: {
+      usePolling: true, // 파일 변경 감지 강화 (특히 Windows 환경에서 유용)
     }
   }
 })
