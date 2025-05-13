6c33fa2f4ca0db115a3ee0aac0b0bec4a3145d51
유석민 <qusgh1027@naver.com>
2025-05-13 23:37:41 +0900

    feat: 성능 테스트 API 개발
    - 댓글 조회 성능 테스트 API 개발
    - SecurityConfig 설정 변경
    - 게시글 pk로 개수 제한해서 댓글 불러오는 로직 추가

--- backend/src/main/java/com/suseok/run/model/repository/CommentRepository.java
+++ backend/src/main/java/com/suseok/run/model/repository/CommentRepository.java
@@ -2,6 +2,8 @@ package com.suseok.run.model.repository;
 
 import com.suseok.run.model.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +12,13 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     List<Comment> findAllByPost_PostId(Long postId);
+
+    @Query(
+            value = "SELECT * FROM comment WHERE post_id = :postId ORDER BY comment_id LIMIT :count",
+            nativeQuery = true
+    )
+    List<Comment> findCommentsNative(
+            @Param("postId") Long postId,
+            @Param("count") int count
+    );
 }
